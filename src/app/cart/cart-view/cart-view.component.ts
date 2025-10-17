aba9ccf3bfe9aac735164476ff379773f903bb3f
Phan Thiện Quang <tomatoquang@gmail.com>
2025-10-17 23:57:22 +0700

    17-10-2025

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-cart-view',
   templateUrl: './cart-view.component.html',
   styleUrls: ['./cart-view.component.css']
 })
-export class CartViewComponent {
+export class CartViewComponent implements OnInit{
 
-  private apiUrl = environment.apiUrl + "/cart";
+  cartItems: Product[] = [];
+  totalPrice: number = 0;
 
-  constructor(private http: HttpClient) { }
+  constructor(private cartService: CartService) {}
 
+  ngOnInit(): void {
+    this.cartService.getCartItems().subscribe(items => {
+      this.cartItems = items;
+      this.totalPrice = this.getTotalPrice();
+    });
+  }
+
+  clearCart(): void {
+    this.cartService.clearCart().subscribe(() => {
+      this.cartItems = [];
+    });
+  }
+
+  getTotalPrice(): number {
+    return this.cartItems.reduce((total, item) => total + item.price, 0);
+  }
 }
